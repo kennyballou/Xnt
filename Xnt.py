3c8ec980c69afe6d0b5724b7f69180ee94cd553b
kennyballou <kballou@onyx.boisestate.edu>
2012-10-10 19:55:29 -0600

    Refactor for use with `entry_point`

--- Xnt.py
+++ Xnt.py
@@ -3,13 +3,15 @@
 import os
 import sys
 
-def main(target=""):
+sys.path.append(os.getcwd())
+
+def main():
+    invokeBuild(sys.argv[1])
+
+def invokeBuild(targetName):
     if not os.path.exists("build.py"):
         sys.stderr.write("There was no build file")
         sys.exit(1)
-    invokeBuild(target)
-
-def invokeBuild(targetName):
     try:
         build = __import__("build", fromlist=[])
         target = getattr(build, targetName)
@@ -17,7 +19,7 @@ def invokeBuild(targetName):
     except AttributeError:
         sys.stderr.write("No such target: " + targetName)
     except:
-        sys.stderr.write("Something more wrong went wrong")
+        print(sys.exc_info())
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main()
