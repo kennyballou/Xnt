823b5b1bf0db8d9a269280b0c5a8ad1e0efae8fc
kennyballou <kballou@onyx.boisestate.edu>
2012-10-24 15:03:33 -0600

    Add ``help`` special 'target'
    Invoking Xnt with ``help`` will now display version and list targets in
    the build file; erring if no build file is found

--- xnt/__init__.py
+++ xnt/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+__version__ = "Xnt 0.2.2"
 
 def target(fn):
     def wrap():
         print(fn.__name__ + ":")
         return fn()
+    wrap.decorator = "target"
     return wrap
--- xnt/xenant.py
+++ xnt/xenant.py
@@ -13,14 +13,17 @@ def main():
     args = sys.argv[1:]
     for arg in args:
         if arg == "version":
-            import xnt
-            print(xnt.__version__)
-            sys.exit()
+            printVersion()
+        elif arg == "help":
+            printVersion()
+            print("\n\n")
+            printTargets()
         elif arg:
             target = arg
+            invokeBuild(target)
         elif not arg:
             target = "default"
-    invokeBuild(target)
+            invokeBuild(target)
     from xnt.tasks import rm
     rm("build.pyc")
 
@@ -34,6 +37,28 @@ def invokeBuild(targetName):
         target()
     except AttributeError:
         logger.warning("There was no target: %s", targetName)
+    except:
+        logger.error(sys.exc_info()[1].message)
+
+def printVersion():
+    import xnt
+    print(xnt.__version__)
+
+def printTargets():
+    if not os.path.exists("build.py"):
+        logger.error("There was no build file")
+        sys.exit(1)
+    try:
+        build = __import__("build", fromlist=[])
+        for f in dir(build):
+            try:
+                fa = getattr(build, f)
+                if fa.decorator == "target":
+                    print(f + ":\n")
+            except AttributeError:
+                pass
+    except AttributeError:
+        pass
     except:
         logger.error(sys.exc_info()[1].message)
 
