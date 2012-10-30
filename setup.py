079ace5fa892a4ffde0d8350c7ddaa9350c3d031
kennyballou <kballou@onyx.boisestate.edu>
2012-10-29 23:25:03 -0600

    Move version forward

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(fname):
 
 setup(
     name="Xnt",
-    version="0.3.0",
+    version="0.3.1",
     author="Kenny Ballou",
     author_email="kennethmgballou@gmail.com",
     url="https://bitbucket.org/devnulltao/xnt",
