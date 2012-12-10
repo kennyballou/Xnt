86f45dc1b973da7093a60b8f8917bd36e002d40a
kennyballou <kballou@onyx.boisestate.edu>
2012-12-10 11:01:48 -0700

    Split tests into separate files

--- xnt/tests/taskmisctests.py
+++ xnt/tests/taskmisctests.py
@@ -23,7 +23,7 @@ import xnt.tasks
 import unittest
 
 
-class TaskTests(unittest.TestCase):
+class TaskMiscTests(unittest.TestCase):
     def setUp(self):
         os.mkdir("temp")
         os.mkdir("temp/testfolder1")
@@ -38,47 +38,6 @@ class TaskTests(unittest.TestCase):
 
     def tearDown(self):
         shutil.rmtree("temp")
-
-    def test_cp(self):
-        xnt.tasks.cp("temp/testfolder1", "temp/testfolder2")
-        self.assertTrue(os.path.exists("temp/testfolder2"))
-        self.assertTrue(os.path.exists("temp/testfolder1"))
-        xnt.tasks.cp("temp/testfile1", "temp/testfile5")
-        self.assertTrue(os.path.exists("temp/testfile5"))
-        self.assertTrue(os.path.exists("temp/testfile1"))
-        with open("temp/testfile5", "r") as testfile:
-            self.assertEqual("this is a test file", testfile.read())
-
-    def test_mv(self):
-        xnt.tasks.mv("temp/testfolder1", "temp/testfolder2")
-        self.assertTrue(os.path.exists("temp/testfolder2"))
-        self.assertFalse(os.path.exists("temp/testfolder1"))
-        xnt.tasks.mv("temp/testfile1", "temp/testfile5")
-        self.assertTrue(os.path.exists("temp/testfile5"))
-        self.assertFalse(os.path.exists("temp/testfile1"))
-        with open("temp/testfile5", "r") as testfile:
-            self.assertEqual("this is a test file", testfile.read())
-
-    def test_mkdir(self):
-        xnt.tasks.mkdir("temp/mynewtestfolder")
-        self.assertTrue(os.path.exists("temp/mynewtestfolder"))
-        self.assertTrue(os.path.exists("temp/testfolder1"))
-        xnt.tasks.mkdir("temp/testfolder1")
-
-    def test_rm(self):
-        xnt.tasks.rm("temp/testfolder1")
-        self.assertFalse(os.path.exists("temp/testfolder1"))
-        xnt.tasks.rm("temp/testfile1")
-        self.assertFalse(os.path.exists("temp/testfile1"))
-        xnt.tasks.rm("temp/testfile2",
-                      "temp/testfile3",
-                      "temp/testfile4")
-        for i in range(1, 5):
-            self.assertFalse(os.path.exists("temp/testfile" + str(i)))
-
-    def test_zip(self):
-        xnt.tasks.zip("temp/testfolder1", "temp/myzip.zip")
-        self.assertTrue(os.path.exists("temp/myzip.zip"))
 
     def test_echo(self):
         xnt.tasks.echo("this is my cool echo", "temp/mynewcoolfile")
