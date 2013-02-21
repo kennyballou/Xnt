0df5ff747ece16fe8dd74f0174c55cad96b7b131
kennyballou <kballou@onyx.boisestate.edu>
2013-02-21 09:31:26 -0700

    Update `build.py` to use new import scheme

--- build.py
+++ build.py
@@ -1,48 +1,46 @@
 #!/usr/bin/env python
 
-from xnt import target
-from xnt.tasks import *
-import xnt.tasks
+import xnt
 
-@target
+@xnt.target
 def clean():
     """Removes Generated folders"""
-    rm("Xnt.egg-info",
-       "build",
-       "docs/build",
-       "dist",
-       "README.html",
-       "**/*.pyc",
-       "**/**/*.pyc",
-       "**__pycache__",
-       "**/**/__pycache__")
+    xnt.rm("Xnt.egg-info",
+           "build",
+           "docs/build",
+           "dist",
+           "README.html",
+           "**/*.pyc",
+           "**/**/*.pyc",
+           "**__pycache__",
+           "**/**/__pycache__")
 
-@target
+@xnt.target
 def build():
-    return setup(["build"])
+    return xnt.setup(["build"])
 
-@target
+@xnt.target
 def test():
     """Tests package"""
     print("Python Tests:")
-    ec1 = setup(["test"])
+    ec1 = xnt.setup(["test"])
     clean()
     print("Python2 Tests:")
-    ec2 = call(["python2", "setup.py", "test"])
+    ec2 = xnt.call(["python2", "setup.py", "test"])
     clean()
     return ec1 | ec2
 
-@target
+@xnt.target
 def install():
     """Install Xnt"""
-    ec = setup(["install", "--user"])
+    ec = xnt.setup(["install", "--user"])
     clean()
     return ec
 
-@target
+@xnt.target
 def doc():
     """
     Create package documentation
     """
     clean()
-    return setup(["build_sphinx"])
+    return xnt.setup(["build_sphinx"])
