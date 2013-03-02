d38128f88e4851c122f9ee799a3e5e26a3c86cdd
kennyballou <kballou@onyx.boisestate.edu>
2013-03-01 21:18:22 -0700

    Move version and dev status

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     },
     install_requires=['distribute',],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
