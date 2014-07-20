87ca7a59e0bb789acca96ae9686e65148b14e4c0
kballou <kballou@devnulllabs.io>
2014-07-20 02:41:23 -0600

    Remove unnecessary import

--- xnt/build/cc.py
+++ xnt/build/cc.py
@@ -20,7 +20,6 @@ Definition of commonly used compilers
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import logging
 from xnt.tasks import __apply__
 from xnt.tasks import __call__
